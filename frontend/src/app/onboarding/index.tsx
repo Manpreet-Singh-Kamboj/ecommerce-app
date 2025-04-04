7aa21291649eeea84c03d170735c3356fe169310
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-04-05 00:26:42 +0530

    Added Liquid Swipe Onboarding Screen

--- frontend/src/app/onboarding/index.tsx
+++ frontend/src/app/onboarding/index.tsx
@@ -1,85 +1,29 @@
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-  ViewabilityConfigCallbackPair,
-  ViewabilityConfigCallbackPairs,
-  ViewToken,
-} from "react-native";
-import React from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import OnboardingItem from "./_components/OnboardingItem";
+import React, { useState } from "react";
+import Slider from "./_components/Slider";
+import Slide from "./_components/Slide";
 import { onboardingData } from "@/constants/onboardingData";
-import PaginationDots from "@/components/PaginationDots";
-import Animated, {
-  useSharedValue,
-  useAnimatedScrollHandler,
-} from "react-native-reanimated";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Pressable } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+export const assets = onboardingData.map(({ picture }) => picture);
 
-const OnboardingPage = () => {
-  const insets = useSafeAreaInsets();
-  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
-  const scrollX = useSharedValue(0);
-  const onScrollHandler = useAnimatedScrollHandler({
-    onScroll: (scrollEvent) => {
-      scrollX.value = scrollEvent.contentOffset.x;
-    },
-  });
-
-  const viewabilityConfigCallbackPairs: ViewabilityConfigCallbackPairs =
-    React.useRef([
-      {
-        viewabilityConfig: {
-          itemVisiblePercentThreshold: 50,
-        },
-        onViewableItemsChanged: ({
-          viewableItems,
-        }: {
-          viewableItems: ViewToken[];
-        }) => {
-          if (viewableItems[0].index !== null) {
-            setCurrentIndex(viewableItems[0].index);
-          }
-        },
-      },
-    ]).current;
-
+const LiquidSwipe = () => {
+  const [index, setIndex] = useState(0);
+  const prev = onboardingData[index - 1];
+  const next = onboardingData[index + 1];
   return (
-    <View
-      style={{
-        backgroundColor: "#f8f8f8",
-        height: "100%",
-        width: "100%",
-        paddingTop: insets.top,
-      }}
-    >
-      <Animated.FlatList
-        data={onboardingData}
-        renderItem={({ item }) => (
-          <OnboardingItem
-            title={item.title}
-            description={item.description}
-            imgPath={item.imgPath}
-          />
-        )}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        bounces={false}
-        keyExtractor={(item) => item.imgPath}
-        onScroll={onScrollHandler}
-        viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs}
-      />
-      <PaginationDots
-        currentIndex={currentIndex}
-        itemsList={onboardingData}
-        scrollX={scrollX}
-      />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slider
+        key={index}
+        index={index}
+        setIndex={setIndex}
+        prev={prev && <Slide slide={prev} />}
+        next={next && <Slide slide={next} />}
+      >
+        <Slide slide={onboardingData[index]!} />
+      </Slider>
+    </GestureHandlerRootView>
   );
 };
 
-export default OnboardingPage;
-
-const styles = StyleSheet.create({});
+export default LiquidSwipe;
--- frontend/src/constants/onboardingData.ts
+++ frontend/src/constants/onboardingData.ts
@@ -1,20 +1,23 @@
 export const onboardingData = [
   {
+    color: "#704F37",
     title: "Seamless Shopping \n Experience",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-    imgPath: require("@assets/images/onboarding-1.png"),
+    picture: require("@assets/images/onboarding-1.png"),
   },
   {
+    color: "#1F2029",
     title: "Swift and Reliable \n Delivery",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-    imgPath: require("@assets/images/onboarding-2.png"),
+    picture: require("@assets/images/onboarding-2.png"),
   },
   {
+    color: "#A45A52",
     title: "Wishlist: Where Fashion \n Dreams Begin",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-    imgPath: require("@assets/images/onboarding-3.png"),
+    picture: require("@assets/images/onboarding-3.png"),
   },
 ];
