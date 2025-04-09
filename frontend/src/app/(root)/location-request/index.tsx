959fabe346fb413f7bd7b837faadc0ace765ad1d
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2025-04-10 00:56:08 +0530

    Added Location Screen

--- frontend/src/app/(root)/location-request/index.tsx
+++ frontend/src/app/(root)/location-request/index.tsx
@@ -1,18 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 import React from "react";
 import SafeAreaWrapper from "@/components/SafeAreaWrapper";
 import LocationIcon from "@/components/icons/LocationIcon";
 import { Colors } from "@/constants/colors";
 import PageHeading from "@/components/PageHeading";
 import PageDescription from "@/components/PageDescription";
+import Button from "@/components/Button";
 
-type Props = {};
-
-const LocationRequest = (props: Props) => {
+const LocationRequest = () => {
+  const { width } = useWindowDimensions();
   return (
     <SafeAreaWrapper>
       <View
         style={{
+          width,
           alignItems: "center",
           justifyContent: "center",
           paddingHorizontal: 10,
@@ -32,6 +33,28 @@ const LocationRequest = (props: Props) => {
           <PageHeading heading="What is Your Location?" fontWeight={"600"} />
           <PageDescription
             description={`We need to know your location in order to suggest \n nearby services.`}
+          />
+        </View>
+        <View style={{ width, gap: 20 }}>
+          <Button
+            text="Allow Location Access"
+            onPress={() => {}}
+            customStyle={{
+              marginHorizontal: 25,
+              borderRadius: 25,
+            }}
+            textColor="#fff"
+            backgroundColor={Colors.secondaryBG}
+          />
+          <Button
+            text="Enter Location Manually"
+            onPress={() => {}}
+            customStyle={{
+              marginHorizontal: 25,
+              borderRadius: 25,
+            }}
+            textColor={Colors.secondaryBG}
+            backgroundColor={Colors.primaryBG}
           />
         </View>
       </View>
