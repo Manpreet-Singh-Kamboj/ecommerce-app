0523c745ae7ce8987c4f3d44f129258377cc223a
Manpreet Singh <manpreetkamboj60.net@gmail.com>
2025-04-26 16:40:33 +0530

    Map Themeing and some issues fixed

--- frontend/src/app/(root)/filters/index.tsx
+++ frontend/src/app/(root)/filters/index.tsx
@@ -1,11 +1,31 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { router, Stack } from "expo-router";
+import ScreensHeader from "@/components/ScreensHeader";
+import { Feather } from "@expo/vector-icons";
 
 type Props = {};
 
 export default function FiltersScreen(props: Props) {
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        name="(root)/filters/index"
+        options={{
+          headerShown: true,
+          header: () => (
+            <ScreensHeader
+              leftIcon={<Feather name="chevron-left" size={24} color="black" />}
+              screenName="Filter"
+              onLeftIconPress={() => {
+                if (router.canGoBack()) {
+                  router.back();
+                }
+              }}
+            />
+          ),
+        }}
+      />
       <ScrollView
         style={styles.scrollContent}
         showsVerticalScrollIndicator={false}
--- frontend/src/app/(root)/notifications/index.tsx
+++ frontend/src/app/(root)/notifications/index.tsx
@@ -1,11 +1,31 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { router, Stack } from "expo-router";
+import ScreensHeader from "@/components/ScreensHeader";
+import { Feather } from "@expo/vector-icons";
 
 type Props = {};
 
 const NotificationsScreen = (props: Props) => {
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        name="(root)/notifications/index"
+        options={{
+          headerShown: true,
+          header: () => (
+            <ScreensHeader
+              leftIcon={<Feather name="chevron-left" size={24} color="black" />}
+              screenName="Notifications"
+              onLeftIconPress={() => {
+                if (router.canGoBack()) {
+                  router.back();
+                }
+              }}
+            />
+          ),
+        }}
+      />
       <ScrollView
         style={styles.scrollContent}
         showsVerticalScrollIndicator={false}
